e3b97d5e3eb05f8138483118acdfc9c9baf3b505
아이스바닐라라떼 <121101681+ljw971125@users.noreply.github.com>
2023-08-03 18:05:48 +0900

    인텔리제이 스프링부트4 요청 응답 클래스 작성

--- Board/src/main/java/com/study/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/config/DatabaseConfig.java
@@ -3,7 +3,6 @@ package com.study.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +36,7 @@ public class DatabaseConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-        //factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+//		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
         return factoryBean.getObject();
     }
 
@@ -45,5 +44,5 @@ public class DatabaseConfig {
     public SqlSessionTemplate sqlSession() throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory());
     }
-}
 
+}
